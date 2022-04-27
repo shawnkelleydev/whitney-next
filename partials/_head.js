3ed894fac4707ac300925779a68e55dc440a0e11
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2022-04-27 06:03:09 -0600

    Add Head partial

---
+++ partials/_head.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function _head() {
+  return (
+    <Head>
+      {/* TODO: Add title / description */}
+      <title>App Title</title>
+      <meta name='description' content='App description' />
+      {/* TODO: Add favicon to public */}
+      <link rel='icon' href='/favicon.ico' />
+      {/* TODO: Add fonts */}
+    </Head>
+  )
+}
