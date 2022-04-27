e8597210348d5e6808ec84a08611b46ec33295f2
Shawn Kelley <skelley@us.ci.org>
2022-04-27 15:48:39 -0600

    Page labels

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import { useRouter } from 'next/router'
 import PageWrapper from 'components/PageWrapper'
 
 export default function Home() {
-  return <PageWrapper></PageWrapper>
+  let { route } = useRouter()
+  route = route.replace('/', '')
+
+  return (
+    <PageWrapper>
+      <h1>home</h1>
+    </PageWrapper>
+  )
 }
--- pages/page1/index.js
+++ pages/page1/index.js
@@ -1,7 +1,16 @@
+import { useRouter } from 'next/router'
+
 import PageWrapper from 'components/PageWrapper'
 
 import styles from './styles.module.css'
 
 export default function Page1() {
-  return <PageWrapper></PageWrapper>
+  let { route } = useRouter()
+  route = route.replace('/', '')
+
+  return (
+    <PageWrapper>
+      <h1>{route}</h1>
+    </PageWrapper>
+  )
 }
