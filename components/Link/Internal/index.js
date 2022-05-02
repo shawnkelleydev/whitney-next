939cc88fde54fd1ee796f1daf2183c7472519142
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2022-05-02 16:15:34 -0600

    Add Link active styles

--- components/Link/Internal/index.js
+++ components/Link/Internal/index.js
@@ -1,9 +1,15 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function External({ styles, text, url }) {
+  let { route } = useRouter()
+  route = route !== '/' ? route.replace('/', '') : 'home'
+
   return (
     <Link href={url}>
-      <a className={styles.a}>{text}</a>
+      <a className={`${styles.a} ${route === text ? styles.active : ''}`}>
+        {text}
+      </a>
     </Link>
   )
 }
--- components/Nav/index.js
+++ components/Nav/index.js
@@ -1,17 +1,13 @@
 import Link from 'components/Link'
-import { useRouter } from 'next/router'
 import { pages } from 'constants/data'
 
 import styles from './styles.module.css'
 
 export default function Nav() {
-  let { route } = useRouter()
-  route = route.replace('/', '')
-
   return (
     <nav className={styles.nav}>
       {pages.map((page, i) => (
-        <Link url={page !== 'home' ? `/${page.toLowerCase()}` : '/'} key={i}>
+        <Link key={i} url={page !== 'home' ? `/${page.toLowerCase()}` : '/'}>
           {page}
         </Link>
       ))}
