a3c752add7dcd2ca0d6fc3c411f9e13fa83a3df7
Shawn Kelley <skelley@us.ci.org>
2022-05-03 20:46:58 -0600

    Add framework for lessons

--- pages/lessons/index.js
+++ pages/lessons/index.js
@@ -1,12 +1,21 @@
 import LessonsMenu from 'components/LessonsMenu'
 import PageWrapper from 'components/PageWrapper'
+import Strip from 'components/Strip'
 
 import styles from './styles.module.css'
 
 export default function PrivateLessons() {
   return (
     <PageWrapper>
-      <h2>Lessons</h2>
+      <Strip>
+        <h2>Private Lessons</h2>
+      </Strip>
+      <section className={styles.quotes}>
+        <blockquote>
+          <q>Considerable technique.</q>
+          <cite>Winston-Salem Journal</cite>
+        </blockquote>
+      </section>
     </PageWrapper>
   )
 }
