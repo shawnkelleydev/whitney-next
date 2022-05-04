e9c30a6a31f3b4918a3764cfe4bf51728f802434
Shawn Kelley <skelley@us.ci.org>
2022-05-03 20:07:08 -0600

    Refactor Banner for mobile friendliness / better clamps

--- components/Banner/index.js
+++ components/Banner/index.js
@@ -1,18 +1,19 @@
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
 import styles from './styles.module.css'
 
 export default function Banner() {
+  const [windowWidth, setWindowWidth] = useState(0)
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth)
+    window.addEventListener('resize', () => setWindowWidth(window.innerWidth))
+  }, [])
+
   return (
     <div className={styles.banner}>
-      <Image
-        alt='dr. whitney kelley sitting, holding a gold flute perpendicular to the ground and smiling'
-        src='/website_banner_2.jpg'
-        width='12000'
-        height='3900'
-        layout='responsive'
-      />
       <div className={styles.overlay}>
         <div>
           <Link href='/'>
@@ -26,6 +27,24 @@ export default function Banner() {
         </div>
         <div />
       </div>
+      {windowWidth >= 500 && (
+        <Image
+          alt='dr. whitney kelley sitting, holding a gold flute perpendicular to the ground and smiling'
+          src='/website_banner_2.jpg'
+          width='12000'
+          height='3900'
+          layout='responsive'
+        />
+      )}
+      {windowWidth < 500 && (
+        <Image
+          alt='dr. whitney kelley sitting, holding a gold flute perpendicular to the ground and smiling'
+          src='/whitney_headshot_2.jpg'
+          width='5184'
+          height='3456'
+          layout='responsive'
+        />
+      )}
     </div>
   )
 }
