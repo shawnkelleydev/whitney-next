506061a56c93882b77b5bc482277a293ef53e32c
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2022-05-06 13:01:52 -0600

    Remove test events

--- constants/schedule.js
+++ constants/schedule.js
@@ -1,23 +1,5 @@
 export const denverPhilSchedule = [
   {
-    title: "World's Fair",
-    performers: ['Denver Philharmonic Orchestra'],
-    url: 'https://denverphilharmonic.org/production/worlds-fair/',
-    imageUrl:
-      'https://denverphilharmonic.org/wp-content/uploads/2021/08/dpo-art2122-d2-worldsfair.jpg',
-    imageAlt: 'blue wireframe globe over yellow splash background',
-    dates: { year: 2022, month: 5, rendered: 'May 25 & 26, 2022' },
-  },
-  {
-    title: 'Pocket Garden',
-    performers: ['Denver Philharmonic Orchestra'],
-    url: 'https://denverphilharmonic.org/production/pocket-garden/',
-    imageUrl:
-      'https://denverphilharmonic.org/wp-content/uploads/2021/08/dpo-art2122-d2-pocketgarden-720x400.jpg',
-    imageAlt: 'outline of a book and wand over a green tree with foliage',
-    dates: { year: 2022, month: 5, rendered: 'May 25 & 26, 2022' },
-  },
-  {
     title: 'Beethoven Celebration',
     performers: ['Denver Philharmonic Orchestra'],
     url: 'https://denverphilharmonic.org/production/beethoven-celebration-2022/',
@@ -27,19 +9,4 @@ export const denverPhilSchedule = [
       'outline of a cupcake filled with yellow and orange balloons and encircled by outward-pointing candles and confetti-like strokes',
     dates: { year: 2022, month: 5, rendered: 'May 25 & 26, 2022' },
   },
-]
-
-export const months = [
-  'january',
-  'february',
-  'march',
-  'april',
-  'may',
-  'june',
-  'july',
-  'august',
-  'september',
-  'october',
-  'november',
-  'december',
 ]
