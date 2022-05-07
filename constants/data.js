a905d767d3e81b6084ac8481be52a4b710aa0813
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2022-05-07 08:59:19 -0600

    Update SEO with location info

--- constants/data.js
+++ constants/data.js
@@ -6,10 +6,11 @@ export const meta = {
   AUTHOR: 'Whitney Kelley',
   CURRENT_YEAR: getYear(new Date()),
   DESCRIPTION:
-    'The digital home of Dr. Whitney Kelley, flute teacher, scholar, and principle flute of the Denver Philharmonic Orchestra.',
+    'The digital home of Dr. Whitney Kelley, flute teacher, scholar, and principle flute of the Denver Philharmonic Orchestra.  Whitney Kelley offers in-person flute lessons in Northglenn, Colorado.',
   DEVELOPER: 'Shawn Kelley',
   DEVELOPER_URL: 'https://www.shawnkelley.dev',
-  KEYWORDS: 'flute, whitney, kelley, lessons, flute lessons',
+  KEYWORDS:
+    'flute, whitney, kelley, lessons, flute lessons, northglenn, thornton, westminster, broomfield',
   TAG: 'Flute',
   TITLE: 'Dr. Whitney Kelley, Flute',
 }
