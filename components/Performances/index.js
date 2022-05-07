fc302b67408bfcb477029bc7895576971a4d2294
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2022-05-07 09:14:26 -0600

    Add if (perfs.length < 1) return null to Performances

--- components/Performances/index.js
+++ components/Performances/index.js
@@ -10,18 +10,19 @@ export default function Performances() {
   const year = todayDate.getFullYear()
   const month = todayDate.getMonth() + 1
 
+  const perfs = denverPhilSchedule.filter(
+    (concert) => concert.dates.year >= year && concert.dates.month >= month
+  )
+
+  if (perfs.length < 1) return null
+
   return (
     <Strip>
       <h2>performances</h2>
       <ul className={styles.performances}>
-        {denverPhilSchedule
-          .filter(
-            (concert) =>
-              concert.dates.year >= year && concert.dates.month >= month
-          )
-          .map((performance, i) => (
-            <Performance key={i} performance={performance} />
-          ))}
+        {perfs.map((performance, i) => (
+          <Performance key={i} performance={performance} />
+        ))}
       </ul>
     </Strip>
   )
