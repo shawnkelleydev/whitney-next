c953292dcdf31ee38f05e666d1139bef8f423e9c
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2022-05-09 20:14:08 -0600

    Add _documents.js

--- partials/_head.js
+++ partials/_head.js
@@ -10,27 +10,6 @@ export default function _head() {
       <meta name='keywords' content={meta.KEYWORDS} />
       {/* TODO: Add custom favicon to public */}
       <link rel='icon' href='/favicon.ico' />
-      <link rel='preconnect' href='https://fonts.googleapis.com' />
-      <link
-        rel='preconnect'
-        href='https://fonts.gstatic.com'
-        crossOrigin='true'
-      />
-      <link
-        href='https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500&display=swap'
-        rel='stylesheet'
-      ></link>
-      {/* experimental fonts */}
-      <link rel='preconnect' href='https://fonts.googleapis.com' />
-      <link
-        rel='preconnect'
-        href='https://fonts.gstatic.com'
-        crossOrigin='true'
-      />
-      <link
-        href='https://fonts.googleapis.com/css2?family=Cinzel&display=swap'
-        rel='stylesheet'
-      />
     </Head>
   )
 }
